bd0be0e011cee3024527dff619e6d6c742db62ee
Andrey Chernyakov <a.chernyakov@comagic.dev>
2023-02-17 12:18:25 +0700

    first working prototype

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # docker-greenplum
+
+
+## Build image
+```
+docker build -f DockerFile -t andruche/greenplum:6.23.1 .
+```
+
+## Usage
+```
+$ docker run --name greenplum -p 5432:5432  -d andruche/greenplum:6.23.1
+```
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+/etc/init.d/ssh start
+sleep 1
+su - gpadmin bash -c 'gpstart -a'
+
+trap "kill %1 && su - gpadmin bash -c 'gpstop -a -M fast' && END=1" INT TERM
+
+tail -f `ls /data/master/gpsne-1/pg_log/gpdb-* | tail -n1` &
+
+#trap
+while [ "$END" == '' ]; do
+  sleep 1
+done
